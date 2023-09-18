92dbb72f87a4080582ee4dc44dc07324cca65e99
da314jones <dwaynejones@pursuit.org>
2023-09-18 14:05:02 -0400

    adoption

---
+++ src/components/AdoptionProcess.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function AdoptionProcess() {
+  return (
+    <div className="adoption-process">
+      <h2>Adoption Process</h2>
+      <p>Follow these steps to adopt a dog:</p>
+      <ol>
+        <li>Fill out the adoption form.</li>
+        <li>Schedule a meet and greet with the dog.</li>
+        <li>Complete a home visit, if required.</li>
+        <li>Sign the adoption contract.</li>
+      </ol>
+      <p>Contact the shelter for more details and assistance.</p>
+    </div>
+  );
+};
+
