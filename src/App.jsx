9b655f57c3576588ca500e7f2a0d5706d1e44a94
elisaul batista <elisaulbatista@pursuit.org>
2023-09-18 18:31:22 -0400

    added adoptionpreceess

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // import './App.css'
 import DogList from './components/DogList'
-
+import AdoptionProcess from './components/AdoptionProcess'
 function App() {
 
   return (
     <>
     <h1>Dog List</h1>
       <DogList />
+      <AdoptionProcess/>
     </>
   )
 }
