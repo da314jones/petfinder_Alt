893ae1e782215a84e83e590309020342d42d31a7
da314jones <dwaynejones@pursuit.org>
2023-09-18 20:23:55 -0400

    api issues

--- src/components/DogList.jsx
+++ src/components/DogList.jsx
@@ -3,17 +3,28 @@ import DogProfile from './DogProfile';
 
 export default function DogList() {
   const [dogs, setDogs] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('/animals') 
-      .then((response) => response.json())
+    fetch('/animals')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then((data) => {
-        setDogs(data); 
+        setDogs(data);
       })
       .catch((error) => {
         console.error('Error fetching animals:', error);
+        setError('Error fetching animals. Please try again later.'); 
       });
   }, []);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
 
   return (
     <div className="dog-list">
